2b94279fae40f855a3da1f7b94dee4464c9594a0
JontheJol <100542245+JontheJol@users.noreply.github.com>
2024-01-05 18:47:38 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+#1 crear 3 clases (cine,salas,funciones)
+#2 minimo 5 propiedades para clase
+#3 clases en archivos separados 
+#4 mis clases no deven llevar print
+#5 cada c;ase deve trabajar p
+#funciones -> salas ->cine 
