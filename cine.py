1d5c948ce909e8d9ee0e8e2f4ec403205e8af309
JontheJol <100542245+JontheJol@users.noreply.github.com>
2024-01-07 21:39:10 -0600

    Add Cine class and test code

--- cine.py
+++ cine.py
@@ -1,1 +1,14 @@
-import salas as s
+import salas 
+
+class Cine:
+    def __init__(self, nombre, direccion,gerente,snack, salas):
+        self.nombre = nombre
+        self.direccion = direccion
+        self.gerente = gerente
+        self.snack = snack
+        self.salas = salas
+
+if __name__ == "__main__":
+    print("Hola mundo")
+    cine = Cine("Cinepolis", "Av. 1", "Juan", "Palomitas", salas.Salas(1, 100, 10, "Juan", salas.Funciones("El señor de los anillos", "B", "Peter Jackson", "Fantasia", 2001)))
+    print(cine.salas.funciones.nomebre)
